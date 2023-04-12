f0858bca105705bea835b0cf11510684118e375c
Sefa IBIS <osmansefaibis@gmail.com>
2023-04-12 16:54:56 +0300

    Project File Structure, Package Dependencies, Initiate UI

---
+++ ItunesSearch-App/ItunesSearch-App/View/Detail/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  ItunesSearch-App
+//
+//  Created by Sefa İbiş on 12.04.2023.
+//
+
+import Foundation
---
+++ ItunesSearch-App/ItunesSearch-App/View/Search/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ItunesSearch-App
+//
+//  Created by Sefa İbiş on 9.04.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var applicationTitleLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var detailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
